f956c5b44fc301495ae84b81a7ed08ddaf881be9
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2016-11-15 21:02:53 -0200

    Adiciona build de router e routes

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ import fs from "fs";
 
 import SequelizeModel from "../templates/sequelize/model";
 import SequelizeMysql from "../templates/sequelize/mysql";
+import Routes from "../templates/routes/routes";
+import Router from "../templates/app/router";
 
 
 export default class App{
@@ -12,14 +14,40 @@ export default class App{
         this.config = config;
     }
 
-    build() {
-        const {src, resource} = this.config;
-        if(src == undefined) throw Error("Destination folder not found");
-        if(resource == undefined) throw Error("Resource not found");
+    buildDatabase(){
+        const { resource } = this.config;
         if(this.config.database == "mysql"){
             this.model = SequelizeModel(resource, "mySQL", this.config.properties);
             this.database = SequelizeMysql(resource);
         }
+    }
+
+    buildRoutes(){
+        const { routes, resource } = this.config;
+        this.routes = new Array();
+        if(routes != undefined){
+            routes.forEach(route => {
+                this.routes.push(
+                    Routes(resource, route.method, route.action, route.additional)
+                );
+            });
+        }
+    }
+
+    buildRouter(){
+        const { routes, middlewares, resource } = this.config;
+        if(middlewares != undefined && routes != undefined){
+            this.router = Router(resource, routes, middlewares);
+        }
+    }
+
+    build() {
+        const { src, resource } = this.config;
+        if(src == undefined) throw Error("Destination folder not found");
+        if(resource == undefined) throw Error("Resource not found");
+        this.buildDatabase();
+        this.buildRoutes();
+        this.buildRouter();
     }
 
 }
